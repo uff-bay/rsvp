4309e69bc2afb22ab1fc3ce4c9d975c73d33a7ce
uff-bay <man@urbanforestfriends.org>
2026-03-19 22:24:10 -0700

    Update webAppUrl to the deployed script URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.RSVP_CONFIG = {
-  webAppUrl: 'PASTE_YOUR_DEPLOYED_APPS_SCRIPT_EXEC_URL_HERE',
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbwMS_J9m06y5QMjV4_Z6RclgKR8HvK5n2H0wy-H4R6t5BEY5SerqNL2qmi9jagMFkIY/exec',
 };
