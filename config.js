07552241b74f4136d8e75e465f09a90a28d87ba8
uff-bay <man@urbanforestfriends.org>
2026-03-21 03:03:16 -0700

    Change Macros URL
    This adds UFF to email names, subject lines, and body

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.RSVP_CONFIG = {
-  webAppUrl: 'https://script.google.com/macros/s/AKfycbxNrA3VCA0XL2xK6V_OsXBW1P8Rs3BDaLx-Rt4yoe15Zvf1l4KoZ5YmQMhTGUaPPO5i/exec',
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbxuNtSJapHrFI-MRjWZ_EJPhCfiyJ6MzO1SiatGozA94zwGr3RWE6ptE9hxefq2OJbx/exec',
 };
